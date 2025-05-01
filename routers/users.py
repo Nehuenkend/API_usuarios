3865ed30b8e3795c8c59afcbb1bfc4095cfca3ac
Nehuen Kendziura <121322725+Nehuenkend@users.noreply.github.com>
2025-04-30 21:56:24 -0300

    Create users.py

---
+++ routers/users.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, HTTPException
+from models.user import User
+
+router = APIRouter()
+
+# implementacion de endpoints db en memoria
+
+users_list = [
+    User(id=1, name="user1", surname="u1", age=11),
+    User(id=2, name="user2", surname="u2", age=22),
+    User(id=3, name="user3", surname="u3", age="33"),
+]
+
+
+@router.get("/users")
+async def users():
+    return users_list
+
+
+@router.get("/user/{id}")  # Path
+async def user(id: int):
+    return search_user(id)
+
+
+@router.get("/user/")  # Query
+async def user(id: int):
+    return search_user(id)
+
+
+@router.post("/user/", response_model=User, status_code=201)  # 201="Created"
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(404, detail="El usuario ya existe")  # 404="Not Found"
+    users_list.append(user)
+    return user
+
+
+@router.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        raise HTTPException(404, detail="No se ha encontrado el usuario")
+    return user
+
+
+@router.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        return {"error": "no se ha eliminado el usuario"}
+
+
+# fucion para ver si hay algun usuario en la id:
+def search_user(id: int):
+    user = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(user)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
